ebe65d9511546b7b542cdc337166fc936f255618
SanoMush <mustofahusni27@gmail.com>
2025-03-23 16:46:07 +0700

    Adding Fitur Edit Copany Statistic and Delete

--- resources/views/admin/statistics/index.blade.php
+++ resources/views/admin/statistics/index.blade.php
@@ -14,7 +14,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
 
-                @forelse($statistics as $statistics)
+                @forelse($statistics as $statistic)
                 <div class="item-card flex flex-row justify-between items-center">
                     <div class="flex flex-row items-center gap-x-3">
                         <img src=" {{Storage::url($statistics->icon)}}" alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
@@ -27,10 +27,12 @@
                         <h3 class="text-indigo-950 text-xl font-bold">{{$statistics->create_at->format('M d, Y')}}</h3>
                     </div>
                     <div class="hidden md:flex flex-row items-center gap-x-3">
-                        <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                        <a href="{{route('admin.statistics.edit', $statistic)}}" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
                             Edit
                         </a>
-                        <form action=" " method="POST"> 
+                        <form action="{{route('admin.statistics.destroy', $statistic)}} " method="POST"> 
+                            @csrf
+                            @method('DELETE')
                             <button type="submit" class="font-bold py-4 px-6 bg-red-700 text-white rounded-full">
                                 Delete
                             </button>
